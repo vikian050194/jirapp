7609f976236f444a225866d26d404fae316207b9
Kirill Vinogradov <vikian050194@gmail.com>
2019-12-19 16:58:42 +0300

    Add footer with countdown timer

--- jira.js
+++ jira.js
@@ -1,3 +1,35 @@
+const addDailyFooter = () => {
+	let countDownTimer = 60;
+
+	let footer = document.createElement("div");
+	footer.id = "daily";
+	footer.style.position = "absolute";
+	footer.style.backgroundColor = "grey";
+	footer.style.bottom = "0px";
+	footer.style.width = "100%";
+	footer.style.height = "10%";
+	footer.style.zIndex = 1000;
+	footer.style.fontSize = "4rem";
+	footer.style.textAlign = "center";
+	footer.innerText = countDownTimer;
+
+	const id = setInterval(() => {
+		footer.innerText = --countDownTimer
+		if(countDownTimer < 0){
+			footer.style.backgroundColor = "red";
+		}
+	}, 1000);
+
+	document.body.appendChild(footer);
+
+	return id;
+};
+
+const removeDailyFooter = (id) => {
+	clearInterval(id);
+	document.querySelector("#daily").remove();
+}
+
 const apply = () => {
 	const filters = [...document.querySelectorAll("a.js-quickfilter-button")].filter(f => f.innerText.length === 2);
 
@@ -5,6 +37,9 @@ const apply = () => {
 		setTimeout(apply, 1000);
 		return;
 	}
+
+	let intervalId = null;
+	const getFilterName = () => filters[index].innerText;
 
 	chrome.runtime.sendMessage("fullscreen");
 	chrome.runtime.sendMessage("zoom");
@@ -19,10 +54,25 @@ const apply = () => {
 	const keys = [...nextKeys, ...previousKeys, ...toggleKeys];
 	
 	let index = filters.indexOf(document.querySelector("a.js-quickfilter-button.ghx-active"));
+	let isFilterEnabled = false;
+
 	const max = filters.length - 1;
-	const click = () => { filters[index].click(); };
+	const click = () => { 
+		filters[index].click();
+		isFilterEnabled = !isFilterEnabled;
+
+		if(isFilterEnabled){
+			intervalId = addDailyFooter(getFilterName());
+		} else{
+			removeDailyFooter(intervalId);
+		}
+	};
 
 	const moveNext = () => {
+		if(!isFilterEnabled){
+			return;
+		}
+
 		click();
 		index++;
 
@@ -34,6 +84,10 @@ const apply = () => {
 	};
 
 	const movePrevious = () => {
+		if(!isFilterEnabled){
+			return;
+		}
+		
 		click();
 		index--;
 
@@ -47,6 +101,8 @@ const apply = () => {
 	if (index === -1) {
 		index = 0;
 		click();
+	} else{
+		intervalId = addDailyFooter(getFilterName());
 	}
 
 	window.addEventListener("keydown", (e) => {
@@ -70,6 +126,7 @@ const apply = () => {
 
 		if (toggleKeys.indexOf(key) !== -1) {
 			click();
+
 			return;
 		}
 	});
